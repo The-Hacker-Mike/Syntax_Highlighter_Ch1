efee7c37bbd3fbbbbe306a45247d48ff10977259
didimelor <A01023785@itesm.mx>
2021-05-18 18:39:39 -0500

    Reads json file

--- Resaltador_de_Sintaxis.rkt
+++ Resaltador_de_Sintaxis.rkt
@@ -5,5 +5,12 @@ Emilio Sánchez
 |#
 
 #lang racket
+(define path_str "C:\Users\diana\Documents\VS/ code\Racket2\Syntax_Highlighter_Ch1\mySpotify.json")
+(string->path path_str)
+(file->list path [Proc #:mode mode-flag]) → (listof any/c)
 
-; actividad
+  path : path-string?
+  Proc : (input-port? . -> . any/c) = read
+  mode-flag : (or/c 'binary 'text) = 'binary
+;actividad
+
